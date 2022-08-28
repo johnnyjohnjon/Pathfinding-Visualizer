e1a0603787e0d9276ac84fb118b1e0057d2a6dcf
John Duan <johnnyduan10@gmail.com>
2022-08-27 17:47:40 -0700

    new

--- src/App.js
+++ src/App.js
@@ -12,9 +12,17 @@ function App() {
         </NavigationBar>
       </div>
       <div>
-        How Does it Work? Click anywhere to create wall. Click go to find shortest path. 
+        <text>       
+          How Does it Work? At its core, a pathfinding algorithm seeks to find the shortest path between two points. 
+          This application visualizes various pathfinding algorithms in action. Click anywhere to create a wall. 
+          Click go to find shortest path. Dijkstra's Algorithm (weighted): the father of pathfinding algorithms; guarantees the shortest path
+          Adding walls and weights Click on the grid to add a wall. Click on the grid while pressing W to add a weight. Generate mazes and patterns from the "Mazes and Patterns" drop-down menu.
+        </text>
       </div>
-      <PathfindingVisualizer></PathfindingVisualizer>
+      <div style={{maxWidth: '100%',maxHeight:"100%"}}>
+        <PathfindingVisualizer>
+        </PathfindingVisualizer>
+      </div>
     </div>
   );
 }
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -43,14 +43,14 @@ export default class PathfindingVisualizer extends Component {
       if (i === visitedNodesInOrder.length) {
         setTimeout(() => {
           this.animateShortestPath(nodesInShortestPathOrder);
-        }, 10 * i);
+        }, 15 * i);
         return;
       }
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
           'node node-visited';
-      }, 10 * i);
+      }, 15 * i);
     }
   }
 
